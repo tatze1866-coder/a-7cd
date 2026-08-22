be73a09cc1df0255284464f52c7807577b3ba207
Tatze66 <tatze1866@gmail.com>
2026-08-22 14:12:10 +0200

    Delete a-7cd directory

--- a-7cd/server.js
+++
@@ -1,3 +0,0 @@
-// Einstiegspunkt im Projekt-Root, damit Hosting-Plattformen (z.B. Render)
-// die App auch über "node server.js" finden. Der eigentliche Code liegt in server/index.js.
-require('./server/index.js');
